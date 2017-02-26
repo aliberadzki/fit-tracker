fce794c050be4b4efe3659f8b4b2c0b0702a68b1
Adam Liberadzki <adam.liberadzki@gmail.com>
2017-02-26 21:17:52 +0100

    added @angular/material

--- fit-tracker-web/src/app/workoutlist/workoutlist.component.ts
+++ fit-tracker-web/src/app/workoutlist/workoutlist.component.ts
@@ -6,11 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./workoutlist.component.css']
 })
 export class WorkoutlistComponent {
-  workouts: Array<Workout> = [];
-
   newWorkoutName : string;
+  workouts : Array<Workout> = [];
+
+
   addWorkout() {
-    this.workouts.push({title: this.newWorkoutName, complete:false});
+    this.workouts.push({
+      id: 0,
+      title: this.newWorkoutName,
+      complete:false
+    });
     this.newWorkoutName = '';
   }
 
@@ -18,9 +23,19 @@ export class WorkoutlistComponent {
     this.workouts.splice(index,1);
   }
 
+  loadWorkouts() {
+    this.workouts = [
+      {id: 1, title: "Trening 1", complete:false},
+      {id: 2, title: "Trening 2", complete:false},
+      {id: 3, title: "Trening 3", complete:false},
+    ];
+
+  }
+
 }
 
 interface Workout {
+  id: number,
   title: string,
   complete: boolean
 }
